58044ca1047f8227c32ace15e06bf0418c9ec14c
Martin Durant <martin.durant@juicemobile.com>
2014-12-02 16:58:19 -0500

    PEP8-ed startup

--- startup.py
+++ startup.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Apr 19 15:47:07 2013
-
-@author: mdurant
+Handy things for interactive sessions.
 """
+from __future__ import print_function, division
+
 try:
     import sip
     sip.setapi('QString', 2)
@@ -18,7 +18,11 @@ and
 plt.tight_layout()
 """
 
-import numpy,scipy,pylab,os,sys,cPickle
+import numpy,scipy,pylab,os,sys
+try:
+    import cPickle
+except ImportError:
+    import pickle as cPickle
 
 from numpy import *
 from pylab import *
@@ -29,7 +33,7 @@ from glob import glob
 from scipy.stats import chi2
 
 def print_err(val,err,e=True):
-    "Produce text of the form 1.25(14)" 
+    "Produce text of the form 1.25(14)"
     Vexponent = int(floor(log10(val)))
     Vmantissa = val / 10**Vexponent
     Eexponent = int(floor(log10(err)))
@@ -46,22 +50,26 @@ def print_err(val,err,e=True):
         line= r"'%."+str(digits)+"f(%i)"+expout+"'%(Vmantissa,Emantissa,Vexponent)"
     return eval(line)
 
-def lorentz(x,x0,gamma):
+
+def lorentz(x, x0, gamma):
     """lorentzian centred at x0, width gamma"""
-    return 1/pi * gamma/( (x-x0)**2+gamma**2 )
+    return 1 / pi * gamma/((x-x0) ** 2 + gamma ** 2)
 
-def gauss(x,x0,sigma):
+
+def gauss(x, x0, sigma):
     """gaussian which works on either scalar or array"""
-    return exp( - ((x-x0) / (sqrt(2)*sigma))**2)
+    return exp(- ((x - x0) / (sqrt(2) * sigma)) ** 2)
 
-def com(x,y):
+
+def com(x, y):
     """Calculate the Nth moment of the data (x,y)"""
     dx = x-x
     dx[0] = diff(x)[0]
     dx[1:] = diff(x)
-    return sum( x * y * dx )/sum( y * dx )
+    return sum(x * y * dx) / sum(y * dx)
 
-def clipped_mean(data,sigmas,verbose=True):
+
+def clipped_mean(data, sigmas, verbose=True):
     """Find the mean, after iteratively rejecting any points
 more than sigmas times the standard deviation from the current
 mean"""
@@ -69,15 +77,17 @@ mean"""
     ave = data.mean()
     if alltrue(abs((data-ave)/sig) < sigmas):
         if verbose:
-            print "Points kept: ",len(data),",  s.d.: ",sig
+            print("Points kept: "+str(len(data))+",  s.d.: "+str(sig))
         return ave
     else:
-        return clipped_mean(data[abs((data-ave)/sig) < sigmas],sigmas,verbose)
+        return clipped_mean(data[abs((data - ave) / sig) < sigmas],
+                            sigmas, verbose)
 
-def RMS(y,sig,axis=None):
+
+def RMS(y, sig, axis=None):
     """Calculate (fractional) RMS for dataset, accounting for extra signal
-    produced by measurement uncertainty "sig" (a number or an array). Perform along
-    "axis", all data if None"""
+    produced by measurement uncertainty "sig" (a number or an array). Perform
+    along "axis", all data if None"""
     if axis is None:
         n = multiply.reduce(y.shape)
     else:
@@ -85,38 +95,40 @@ def RMS(y,sig,axis=None):
     ybar = y.mean(axis=axis)
     return (((y-ybar)**2-sig**2).sum(axis=axis)/n)**0.5/ybar
 
-def weighted_mean(x,dx):
+
+def weighted_mean(x, dx):
     """Mean of x weighted by the uncertainties in dx. Returns mean
 and uncertainty (assumes Gaussian uncertainties)."""
     x = array(x)
     dx = array(dx)
-    assert len(x)==len(dx)
-    mean = sum(x/dx**2)/sum(1/dx**2)
-    err = sqrt(1/sum(1/dx**2))
-    return mean,err
+    assert len(x) == len(dx)
+    mean = sum(x / dx**2) / sum(1 / dx**2)
+    err = sqrt(1 / sum(1 / dx**2))
+    return mean, err
+
 
 def rebin(a, *args):
     """rebin array a by integer factors. *args are the new dimenisons.
     Use congrid for interpolation."""
     shape = a.shape
     lenShape = len(shape)
-    factor = asarray(shape)/asarray(args)
+    factor = asarray(shape) / asarray(args)
     evList = ['a.reshape('] + \
-             ['args[%d],factor[%d],'%(i,i) for i in range(lenShape)] + \
-             [')'] + ['.mean(%d)'%(i+1) for i in range(lenShape)]
+             ['args[%d],factor[%d],' % (i, i) for i in range(lenShape)] + \
+             [')'] + ['.mean(%d)' % (i+1) for i in range(lenShape)]
     return eval(''.join(evList))
 
-def splineinterp(x,y,newx,order=3,smooth=0.1):
+
+def splineinterp(x, y, newx, order=3, smooth=0.1):
     """find values of y at the newx points by spline interpolation of (x,y).
     Values outside the range of x are set to the extreme values of y"""
     from scipy import interpolate as IN
-    tck = IN.splrep(x,y,k=order,s=smooth)
-    newy = IN.splev(newx,tck)
+    tck = IN.splrep(x, y, k=order, s=smooth)
+    newy = IN.splev(newx, tck)
     return newy
 
 
-
-def lininterp(x,y,newx):
+def lininterp(x, y, newx):
     """find values of y at the newx points by linear interpolation of (x,y).
     Values outside the range of x are set to the extreme values of y"""
     order = argsort(x)
@@ -124,16 +136,18 @@ def lininterp(x,y,newx):
     x = x[order]
     newy = newx-newx
     for i in range(len(newx)):
-        if newx[i]<=x.min():
-            newy[i]=y[0]
+        if newx[i] <= x.min():
+            newy[i] = y[0]
             continue
-        if newx[i]>=x.max():
-            newy[i]=y[-1]
+        if newx[i] >= x.max():
+            newy[i] = y[-1]
             continue
-        before = find(newx[i]-x>0)[-1]
+        before = find(newx[i] - x > 0)[-1]
         gap = x[before+1]-x[before]
-        newy[i] = (x[before]-newx[i])*y[before+1]/-gap + (x[before+1]-newx[i])*y[before]/gap
+        newy[i] = ((x[before] - newx[i]) * y[before + 1] / -gap + (x[before + 1] -
+                  newx[i]) * y[before] / gap)
     return newy
+
 
 def primefactors(x):
     """Returs the prime factorisation of x, as a list of primes.
@@ -142,12 +156,13 @@ def primefactors(x):
     results = []
     prime = 2
     while prime <= x:
-        if x%prime == 0:
+        if x % prime == 0:
             results.append(prime)
-            x=x/prime
+            x = x / prime
         else:
             prime = prime+1
     return results
+
 
 def iter_pickle(filename):
     f = open(filename)
@@ -156,21 +171,24 @@ def iter_pickle(filename):
             yield cPickle.load(f)
         except EOFError:
             f.close()
-            raise StopIteration # or "break"
-            
-def imshowz(im,**kwargs):
+            raise StopIteration  # or "break"
+
+
+def imshowz(im, **kwargs):
     """Show image normally as with imshow, but provide the data value
     under the cursor in the status area."""
-    out = imshow(im,**kwargs)    
-    ymax,xmax = im.shape
-    def format_coord(x,y):
-        if x<0 or y<0 or x>xmax or y>ymax:
-            return "x=%6.3f      y=%6.3f"%(x,y)
-        z = im[round(y),round(x)]
-        return "x=%6.3f      y=%6.3f      z=%6.4e"%(x,y,z)
+    out = imshow(im, **kwargs)
+    ymax, xmax = im.shape
+
+    def format_coord(x, y):
+        if x < 0 or y < 0 or x > xmax or y > ymax:
+            return "x=%6.3f      y=%6.3f" % (x, y)
+        z = im[round(y), round(x)]
+        return "x=%6.3f      y=%6.3f      z=%6.4e" % (x, y, z)
     out.axes.format_coord = format_coord
     return out
-    
+
+
 def is_number(string):
     try:
         x = float(string)
